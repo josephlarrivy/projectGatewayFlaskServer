05350e7ea6c923ebe5fdbb545f7e97d156d04f2d
josephlarrivy <joseph.larrivy@gmail.com>
2024-09-22 17:11:19 -0500

    db_init.sql file added and working to create and seed database

--- db_init.sql
+++ db_init.sql
@@ -1,4 +1,16 @@
--- psql -U postgres -d gatewayprojectflaskdatabase -f db_init.sql
+-- TO CREATE THIS DATABASE RUN: psql -h localhost -U josephlarrivy -d postgres -f db_init.sql
+-- TO CONNECT TO PSQL IN THE TERMINAL AND VIEW THIS DATABASE RUN: psql -h localhost -U josephlarrivy -d gatewayprojectflaskdatabase 
+
+-- Connect to the default "postgres" database (or another database that is not the one you want to drop)
+\c postgres
+
+-- Drop the existing database
+DROP DATABASE IF EXISTS gatewayprojectflaskdatabase;
+
+-- Recreate the database
+CREATE DATABASE gatewayprojectflaskdatabase;
+
+\c gatewayprojectflaskdatabase;
 
 DROP TABLE IF EXISTS users CASCADE;
 DROP TABLE IF EXISTS sports CASCADE;
@@ -11,3 +23,6 @@ CREATE TABLE sports (
     sport_id SERIAL PRIMARY KEY,
     sport_name VARCHAR(100) UNIQUE NOT NULL
 );
+
+-- Insert some initial data into the Users table
+INSERT INTO sports (sport_name) VALUES ('hockey');
